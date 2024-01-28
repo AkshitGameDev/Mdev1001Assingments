28b774638bc46be3e7bae3beaef25fb79c213cb2
priyankathomxs <200587559@student.georgianc.on.ca>
2024-01-28 18:59:06 -0500

    commit 2 Priyanka

--- Assingment1/Assingment1.session.sql
+++ Assingment1/Assingment1.session.sql
@@ -1,10 +1,18 @@
-SELECT *
-   FROM movies;
+-- SELECT *
+--    FROM movies; 
 
-SELECT * 
-   FROM movies
-   WHERE criticsRating > 8;
+-- SELECT *
+--    FROM movies
+--    WHERE criticsRating > 8;
 
-SELECT title 
-    FROM movies
-    WHERE title LIKE 'B%';
+-- SELECT title
+--     FROM movies
+--     WHERE title LIKE 'B%';
+
+-- SELECT *
+--  FROM movies
+--  WHERE year < 2000 AND criticsRating >= 8.2;
+
+-- SELECT *
+--    FROM movies
+--    WHERE year BETWEEN 1990 AND 2010 AND mpaRating = 'R';
